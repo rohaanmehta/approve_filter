a462487cbc906969d5ebe7b036089b45e2645707
Amit Pashte <amitpashte2007@gmail.com>
2023-02-18 10:27:48 +0530

    w

--- app/Views/keyword.php
+++ app/Views/keyword.php
@@ -14,21 +14,21 @@
                     <div class="col-lg-12 col-md-12 col-xs-12 mb-12 mb-3">
                         <div class="form-group">
                             <label for="form-label">Brand Restrictions </label>
-                            <textarea rows='5' class="form-control" name="brand_res" id="brand_res"><?php if(isset($update)){ echo $update[0]['brand_res'];}?></textarea>
+                            <textarea rows='5' onkeypress="return event.charCode != 13" class="form-control" name="brand_res" id="brand_res"><?php if(isset($update)){ echo $update[0]['brand_res'];}?></textarea>
                             <div class="text-danger" id="error-brand_res"></div>
                         </div>
                     </div>
                     <div class="col-lg-12 mb-3 col-md-12 col-xs-12 mb-12">
                         <div class="form-group">
                             <label for="form-label">Item Restrictions </label>
-                            <textarea rows='5' class="form-control" name="item_res" id="item_res"><?php if(isset($update)){ echo $update[0]['item_res'];}?></textarea>
+                            <textarea rows='5' onkeypress="return event.charCode != 13" class="form-control" name="item_res" id="item_res"><?php if(isset($update)){ echo $update[0]['item_res'];}?></textarea>
                             <div class="text-danger" id="error-item_res"></div>
                         </div>
                     </div> 
                     <div class="col-lg-12 mb-3 col-md-12 col-xs-12 mb-12">
                         <div class="form-group">
                             <label for="form-label">Shipping Restrictions </label>
-                            <textarea rows='5' class="form-control" name="shipping_res" id="shipping_res"><?php if(isset($update)){ echo $update[0]['shipping_res'];}?></textarea>
+                            <textarea rows='5' onkeypress="return event.charCode != 13" class="form-control" name="shipping_res" id="shipping_res"><?php if(isset($update)){ echo $update[0]['shipping_res'];}?></textarea>
                             <div class="text-danger" id="error-shipping_res"></div>
                         </div>
                     </div>
